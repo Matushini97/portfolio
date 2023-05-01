83282acbe341cec52a34b2e647caead9789945fb
Ilya Matsiushyn <matushin.97@gmail.com>
2023-05-01 18:53:49 +0300

    feat: add tg icon to footer, add _blank to <a>

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,6 +1,7 @@
 import s from "./Footer.module.scss";
 import github from "../../../assets/github-icon.svg";
 import linkedin from "../../../assets/linkedin-icon.svg";
+import tg from "../../../assets/tg-icon.svg"
 
 const Footer = () => {
   return (
@@ -10,11 +11,14 @@ const Footer = () => {
           <h2>Copyright © 2023. All rights are reserved</h2>
         </div>
         <div className={s.iconWrapper}>
-          <a href="https://github.com/Matushini97">
+          <a href="https://github.com/Matushini97" target="_blank">
             <img src={github} alt="github" className={s.icon} />
           </a>
-          <a href="https://www.linkedin.com/in/ilya-matsiushyn-165602229/">
+          <a href="https://www.linkedin.com/in/ilya-matsiushyn-165602229/" target="_blank">
             <img src={linkedin} alt="linkedin" className={s.icon} />
+          </a>
+          <a href="https://t.me/Matushini" target="_blank">
+            <img src={tg} alt="tg" className={s.icon} />
           </a>
         </div>
       </div>
